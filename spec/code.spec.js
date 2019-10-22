6b995f6a52efea865bf16ea2d72f397ce73788bf
Olha Virolainen <olga.shukel@gmail.com>
2019-10-22 16:51:00 +0300

    emit `end` if no data

--- spec/code.spec.js
+++ spec/code.spec.js
@@ -30,7 +30,7 @@ describe('code test', () => {
       await action.process.call(self, {}, { code });
       const result = emitter.emit.getCall(0).args[1];
       expect(result.body.name).equal('Name');
-      const testEnv = process.env.TEST_ENV;
+      const testEnv = process.env.TEST_ENV || 'test';
       expect(testEnv).equal('test');
     });
   });
